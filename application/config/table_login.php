e813f7246b04d2c48b559f0cf2f7cc0946e7788c
saifultitu90 <saifultitu90@gmail.com>
2017-07-03 12:06:54 +0600

    table_login updated

--- application/config/table_login.php
+++ application/config/table_login.php
@@ -1,11 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 $config['table_login_setup_user'] = 'arm_login_2017_07.setup_user';
 $config['table_login_setup_user_info'] = 'arm_login_2017_07.setup_user_info';
 $config['table_setup_offices'] = 'arm_login_2017_07.setup_offices';
 $config['table_setup_designation'] = 'arm_login_2017_07.setup_designation';
-$config['table_login_setup_department'] = 'arm_login_2017_07.login_setup_department';
+$config['table_setup_department'] = 'arm_login_2017_07.login_setup_department';
 $config['table_login_setup_user_type'] = 'arm_login_2017_07.setup_user_type';
 $config['table_login_setup_users_other_sites'] = 'arm_login_2017_07.setup_users_other_sites';
 $config['table_login_other_sites_visit'] = 'arm_login_2017_07.other_sites_visit';
