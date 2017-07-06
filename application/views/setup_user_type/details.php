99618b9d8e2e451ea5db4edc3a31e0605a964499
saifultitu90 <saifultitu90@gmail.com>
2017-07-06 10:57:17 +0600

    setup user type details view updated

--- application/views/setup_user_type/details.php
+++ application/views/setup_user_type/details.php
@@ -26,7 +26,7 @@ $CI->load->view('action_buttons',array('action_buttons'=>$action_buttons));
     </div>
     <div class="row show-grid">
         <div class="col-xs-4">
-            <label class="control-label pull-right"><?php echo $this->lang->line('LABEL_ADDRESS');?><span style="color:#FF0000">*</span></label>
+            <label class="control-label pull-right"><?php echo $this->lang->line('LABEL_REMARKS');?><span style="color:#FF0000">*</span></label>
         </div>
         <div class="col-sm-4 col-xs-8">
             <label class="control-label"><?php echo $user_type['remarks'];?></label>
