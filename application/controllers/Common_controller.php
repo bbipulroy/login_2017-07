94f7cd629cf1246bb140c9d0a22dc52b890f61b7
Bipul Roy <bbipulroy@users.noreply.github.com>
2017-07-08 15:55:47 +0700

    Customize Label code added in Common_controller.php

--- application/controllers/Common_controller.php
+++ application/controllers/Common_controller.php
@@ -20,6 +20,10 @@ class Common_controller extends Root_Controller
         {
             $html_container_id=$this->input->post('html_container_id');
         }
+        if($this->input->post('select_label'))
+        {
+            $data['select_label']=$this->input->post('select_label');
+        }
         $data['items']=Query_helper::get_info($this->config->item('table_setup_location_upazillas'),array('id value','name text'),array('district_id ='.$district_id,'status ="'.$this->config->item('system_status_active').'"'),0,0,array('ordering ASC'));
         $ajax['status']=true;
         $ajax['system_content'][]=array("id"=>$html_container_id,"html"=>$this->load->view("dropdown_with_select",$data,true));
@@ -34,6 +38,10 @@ class Common_controller extends Root_Controller
         if($this->input->post('html_container_id'))
         {
             $html_container_id=$this->input->post('html_container_id');
+        }
+        if($this->input->post('select_label'))
+        {
+            $data['select_label']=$this->input->post('select_label');
         }
         $data['items']=Query_helper::get_info($this->config->item('table_setup_location_unions'),array('id value','name text'),array('upazilla_id ='.$upazilla_id,'status ="'.$this->config->item('system_status_active').'"'),0,0,array('ordering ASC'));
         $ajax['status']=true;
@@ -51,6 +59,10 @@ class Common_controller extends Root_Controller
         if($this->input->post('html_container_id'))
         {
             $html_container_id=$this->input->post('html_container_id');
+        }
+        if($this->input->post('select_label'))
+        {
+            $data['select_label']=$this->input->post('select_label');
         }
         $data['items']=Query_helper::get_info($this->config->item('table_setup_classification_varieties'),array('id value','name text'),array('crop_type_id ='.$crop_type_id,'status ="'.$this->config->item('system_status_active').'"'),0,0,array('ordering ASC'));
         $ajax['status']=true;
