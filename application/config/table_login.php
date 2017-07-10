3df7d78ab0c568c6284db5bf1aba5c1d75065e75
saifultitu90 <saifultitu90@gmail.com>
2017-07-10 11:10:24 +0600

    table_login updated

--- application/config/table_login.php
+++ application/config/table_login.php
@@ -2,14 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $config['table_login_setup_user'] = 'arm_login_2017_07.setup_user';
 $config['table_login_setup_user_info'] = 'arm_login_2017_07.setup_user_info';
-$config['table_setup_offices'] = 'arm_login_2017_07.setup_offices';
+$config['table_login_setup_offices'] = 'arm_login_2017_07.setup_offices';
 $config['table_login_setup_designation'] = 'arm_login_2017_07.setup_designation';
-$config['table_setup_department'] = 'arm_login_2017_07.login_setup_department';
+$config['table_login_setup_department'] = 'arm_login_2017_07.login_setup_department';
 $config['table_login_setup_user_type'] = 'arm_login_2017_07.setup_user_type';
 $config['table_login_setup_users_other_sites'] = 'arm_login_2017_07.setup_users_other_sites';
 $config['table_login_other_sites_visit'] = 'arm_login_2017_07.other_sites_visit';
-$config['table_setup_company'] = 'arm_login_2017_07.login_setup_company';
-$config['table_setup_users_company'] = 'arm_login_2017_07.login_setup_users_company';
+$config['table_login_setup_company'] = 'arm_login_2017_07.login_setup_company';
+$config['table_login_setup_users_company'] = 'arm_login_2017_07.login_setup_users_company';
 //location setup
 $config['table_setup_location_divisions'] = 'arm_login_2017_07.login_divisions';
 $config['table_setup_location_zones'] = 'arm_login_2017_07.login_zones';
