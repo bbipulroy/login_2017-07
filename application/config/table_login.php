b9c319a091795f30c3f300206f3103d69752664c
saifultitu90 <saifultitu90@gmail.com>
2017-07-15 08:17:31 +0600

    table_login updated

--- application/config/table_login.php
+++ application/config/table_login.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$config['table_login_setup_user'] = 'arm_login_2017_07.setup_user';
-$config['table_login_setup_user_info'] = 'arm_login_2017_07.setup_user_info';
-$config['table_login_setup_offices'] = 'arm_login_2017_07.setup_offices';
-$config['table_login_setup_designation'] = 'arm_login_2017_07.setup_designation';
+$config['table_login_setup_user'] = 'arm_login_2017_07.login_setup_user';
+$config['table_login_setup_user_info'] = 'arm_login_2017_07.login_setup_user_info';
+$config['table_login_setup_offices'] = 'arm_login_2017_07.login_setup_offices';
+$config['table_login_setup_designation'] = 'arm_login_2017_07.login_setup_designation';
 $config['table_login_setup_department'] = 'arm_login_2017_07.login_setup_department';
-$config['table_login_setup_user_type'] = 'arm_login_2017_07.setup_user_type';
-$config['table_login_setup_users_other_sites'] = 'arm_login_2017_07.setup_users_other_sites';
-$config['table_login_other_sites_visit'] = 'arm_login_2017_07.other_sites_visit';
+$config['table_login_setup_user_type'] = 'arm_login_2017_07.login_setup_user_type';
+$config['table_login_setup_users_other_sites'] = 'arm_login_2017_07.login_setup_users_other_sites';
+$config['table_login_other_sites_visit'] = 'arm_login_2017_07.login_other_sites_visit';
 $config['table_login_setup_company'] = 'arm_login_2017_07.login_setup_company';
 $config['table_login_setup_users_company'] = 'arm_login_2017_07.login_setup_users_company';
 //location setup
@@ -26,6 +26,7 @@ $config['table_login_setup_classification_vpack_size'] = 'arm_login_2017_07.logi
 $config['table_login_setup_classification_variety_category'] = 'arm_login_2017_07.login_setup_classification_variety_category';
 $config['table_login_setup_classification_hybrid'] = 'arm_login_2017_07.login_setup_classification_hybrid';
 $config['table_login_setup_variety_principals'] = 'arm_login_2017_07.login_setup_variety_principals';
+$config['table_login_setup_classification_variety_price'] = 'arm_login_2017_07.login_setup_classification_variety_price';
 //customer setup
 $config['table_login_csetup_cus_type'] = 'arm_login_2017_07.login_csetup_customer_type';
 $config['table_login_csetup_incharge'] = 'arm_login_2017_07.login_csetup_incharge';
