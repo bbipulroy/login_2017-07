aac08770cd155c8df628deb7cdd65d58c67a4d02
S. M. Imtiaz Hasan <smimtiaz56@gmail.com>
2017-07-18 08:07:11 +0600

    new table added

--- application/config/table_login.php
+++ application/config/table_login.php
@@ -39,4 +39,5 @@ $config['table_login_csetup_cus_document'] = 'arm_login_2017_07.login_csetup_cus
 $config['table_login_basic_setup_fiscal_year'] = 'arm_login_2017_07.login_basic_setup_fiscal_year';
 $config['table_login_basic_setup_competitor'] = 'arm_login_2017_07.login_basic_setup_competitor';
 $config['table_login_basic_setup_principal'] = 'arm_login_2017_07.login_basic_setup_principal';
-
+// transfer
+$config['table_old_csetup_customer'] = 'arm_ems.ems_csetup_customers';
