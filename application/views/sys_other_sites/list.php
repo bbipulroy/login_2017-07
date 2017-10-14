c47ca39d226442f31201721555a96ae8b72f0d2d
Bipul Roy <bbipulroy@users.noreply.github.com>
2017-10-14 09:32:52 +0600

    Added these two options in jqxGrid
    enablebrowserselection: true,
    columnsreorder: true

--- application/views/sys_other_sites/list.php
+++ application/views/sys_other_sites/list.php
@@ -109,6 +109,8 @@ $CI->load->view('action_buttons',array('action_buttons'=>$action_buttons));
                 pagesize:50,
                 pagesizeoptions: ['20', '50', '100', '200','300','500'],
                 selectionmode: 'singlerow',
+                enablebrowserselection: true,
+                columnsreorder: true,
                 altrows: true,
                 autoheight: true,
                 columns: [
